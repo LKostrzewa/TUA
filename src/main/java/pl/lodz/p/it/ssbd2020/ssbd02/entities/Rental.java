75020a7475aac8a6fa88a8bc9939a694b38b064b
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2020-03-31 21:26:01 +0200

    dodano wygenerowane klasy dla moj

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/entities/Rental.java
@@ -0,0 +1,209 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2020.ssbd02.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Lukasz
+ */
+@Entity
+@Table(name = "rental")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Rental.findAll", query = "SELECT r FROM Rental r"),
+    @NamedQuery(name = "Rental.findById", query = "SELECT r FROM Rental r WHERE r.id = :id"),
+    @NamedQuery(name = "Rental.findByVersion", query = "SELECT r FROM Rental r WHERE r.version = :version"),
+    @NamedQuery(name = "Rental.findByBeginDate", query = "SELECT r FROM Rental r WHERE r.beginDate = :beginDate"),
+    @NamedQuery(name = "Rental.findByEndDate", query = "SELECT r FROM Rental r WHERE r.endDate = :endDate"),
+    @NamedQuery(name = "Rental.findByPrice", query = "SELECT r FROM Rental r WHERE r.price = :price")})
+public class Rental implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "version")
+    private long version;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Column(name = "business_key")
+    private Object businessKey;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "begin_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date beginDate;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "end_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "price")
+    private BigDecimal price;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "rentalId")
+    private Opinion opinion;
+    @JoinColumn(name = "rental_status_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private RentalStatus rentalStatusId;
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private User userId;
+    @JoinColumn(name = "yacht_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Yacht yachtId;
+
+    public Rental() {
+    }
+
+    public Rental(Long id) {
+        this.id = id;
+    }
+
+    public Rental(Long id, long version, Object businessKey, Date beginDate, Date endDate, BigDecimal price) {
+        this.id = id;
+        this.version = version;
+        this.businessKey = businessKey;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public Object getBusinessKey() {
+        return businessKey;
+    }
+
+    public void setBusinessKey(Object businessKey) {
+        this.businessKey = businessKey;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Opinion getOpinion() {
+        return opinion;
+    }
+
+    public void setOpinion(Opinion opinion) {
+        this.opinion = opinion;
+    }
+
+    public RentalStatus getRentalStatusId() {
+        return rentalStatusId;
+    }
+
+    public void setRentalStatusId(RentalStatus rentalStatusId) {
+        this.rentalStatusId = rentalStatusId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Yacht getYachtId() {
+        return yachtId;
+    }
+
+    public void setYachtId(Yacht yachtId) {
+        this.yachtId = yachtId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rental)) {
+            return false;
+        }
+        Rental other = (Rental) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pl.lodz.p.it.ssbd2020.ssbd02.entities.Rental[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/entities/Yacht.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2020.ssbd02.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Lukasz
+ */
+@Entity
+@Table(name = "yacht")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Yacht.findAll", query = "SELECT y FROM Yacht y"),
+    @NamedQuery(name = "Yacht.findById", query = "SELECT y FROM Yacht y WHERE y.id = :id"),
+    @NamedQuery(name = "Yacht.findByVersion", query = "SELECT y FROM Yacht y WHERE y.version = :version"),
+    @NamedQuery(name = "Yacht.findByName", query = "SELECT y FROM Yacht y WHERE y.name = :name"),
+    @NamedQuery(name = "Yacht.findByProductionYear", query = "SELECT y FROM Yacht y WHERE y.productionYear = :productionYear"),
+    @NamedQuery(name = "Yacht.findByPriceMultipler", query = "SELECT y FROM Yacht y WHERE y.priceMultipler = :priceMultipler"),
+    @NamedQuery(name = "Yacht.findByCondition", query = "SELECT y FROM Yacht y WHERE y.condition = :condition"),
+    @NamedQuery(name = "Yacht.findByAvgRating", query = "SELECT y FROM Yacht y WHERE y.avgRating = :avgRating")})
+public class Yacht implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "version")
+    private BigInteger version;
+    @Lob
+    @Column(name = "business_key")
+    private Object businessKey;
+    @Size(max = 32)
+    @Column(name = "name")
+    private String name;
+    @Column(name = "production_year")
+    private Integer productionYear;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "price_multipler")
+    private BigDecimal priceMultipler;
+    @Size(max = 2048)
+    @Column(name = "condition")
+    private String condition;
+    @Column(name = "avg_rating")
+    private BigDecimal avgRating;
+    @JoinColumn(name = "current_port_id", referencedColumnName = "id")
+    @ManyToOne
+    private Port currentPortId;
+    @JoinColumn(name = "yacht_model_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private YachtModel yachtModelId;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "yachtId")
+    private Collection<Rental> rentalCollection;
+
+    public Yacht() {
+    }
+
+    public Yacht(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigInteger getVersion() {
+        return version;
+    }
+
+    public void setVersion(BigInteger version) {
+        this.version = version;
+    }
+
+    public Object getBusinessKey() {
+        return businessKey;
+    }
+
+    public void setBusinessKey(Object businessKey) {
+        this.businessKey = businessKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(Integer productionYear) {
+        this.productionYear = productionYear;
+    }
+
+    public BigDecimal getPriceMultipler() {
+        return priceMultipler;
+    }
+
+    public void setPriceMultipler(BigDecimal priceMultipler) {
+        this.priceMultipler = priceMultipler;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public BigDecimal getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(BigDecimal avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public Port getCurrentPortId() {
+        return currentPortId;
+    }
+
+    public void setCurrentPortId(Port currentPortId) {
+        this.currentPortId = currentPortId;
+    }
+
+    public YachtModel getYachtModelId() {
+        return yachtModelId;
+    }
+
+    public void setYachtModelId(YachtModel yachtModelId) {
+        this.yachtModelId = yachtModelId;
+    }
+
+    @XmlTransient
+    public Collection<Rental> getRentalCollection() {
+        return rentalCollection;
+    }
+
+    public void setRentalCollection(Collection<Rental> rentalCollection) {
+        this.rentalCollection = rentalCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Yacht)) {
+            return false;
+        }
+        Yacht other = (Yacht) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pl.lodz.p.it.ssbd2020.ssbd02.entities.Yacht[ id=" + id + " ]";
+    }
+    
+}
