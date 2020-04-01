84dfe617ef93c2ae488b178b7eb91576891752d8
student <student@devel>
2020-04-01 18:01:31 +0200

    wygenerowano fasady dla MOJ

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/facade/UserFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2020.ssbd02.moj.facade;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2020.ssbd02.entities.User;
+
+/**
+ *
+ * @author student
+ */
+@Stateless
+public class UserFacade extends AbstractFacade<User> {
+
+    @PersistenceContext(unitName = "ssbd02mokPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserFacade() {
+        super(User.class);
+    }
+    
+}
