0e3c1f05f1d7304f5c50b49dead19657f037af01
Przemek <216827@edu.p.lodz.pl>
2020-04-10 16:03:59 +0200

    megowanie

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/endpoints/UserEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/endpoints/UserEndpoint.java
@@ -3,21 +3,16 @@ package pl.lodz.p.it.ssbd2020.ssbd02.mok.endpoints;
 
 
 import pl.lodz.p.it.ssbd2020.ssbd02.entities.User;
-import pl.lodz.p.it.ssbd2020.ssbd02.entities.UserAccessLevel;
+
 import pl.lodz.p.it.ssbd2020.ssbd02.mok.dtos.UserDTO;
-import pl.lodz.p.it.ssbd2020.ssbd02.mok.facades.AccessLevelFacade;
-import pl.lodz.p.it.ssbd2020.ssbd02.mok.facades.UserAccessLevelFacade;
-import pl.lodz.p.it.ssbd2020.ssbd02.mok.facades.UserFacade;
-import pl.lodz.p.it.ssbd2020.ssbd02.utils.BCryptPasswordHash;
+import pl.lodz.p.it.ssbd2020.ssbd02.mok.managers.UserManager;
 import pl.lodz.p.it.ssbd2020.ssbd02.utils.LoggerInterceptor;
 
 
 import javax.ejb.*;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Stateful
 @LocalBean
