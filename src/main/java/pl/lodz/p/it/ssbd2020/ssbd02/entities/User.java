42dabb7a56b9735892f082820634ecd3f291dfd7
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2020-04-18 14:44:12 +0200

    setId() zostaje usunięte poprawa edycji nastąpi później

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/entities/User.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/entities/User.java
@@ -113,10 +113,6 @@ public class User implements Serializable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public long getVersion() {
         return version;
     }
