d76cf92d3538715d45521a6558fc7c68b35cf08e
Przemysław Łⱥpiński <216827@edu.p.lodz.pl>
2020-04-23 09:09:25 +0200

    dodanie spowrotem setEmail w ListUserDto

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/dtos/ListUsersDto.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/dtos/ListUsersDto.java
@@ -29,6 +29,10 @@ public class ListUsersDto implements Comparable<ListUsersDto> {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public boolean isLocked() {
         return locked;
     }
