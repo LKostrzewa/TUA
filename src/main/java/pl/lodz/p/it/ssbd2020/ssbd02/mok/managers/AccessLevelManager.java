7854a224e54d035a13132cb9b5d0434442fca0ef
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2020-05-18 00:47:09 +0200

    Utworzono interfejsy dla endpointów mok oraz dodano implementacje nieużywanych metod z AbstractFacade z @DenyAll

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/managers/AccessLevelManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/mok/managers/AccessLevelManager.java
@@ -5,8 +5,12 @@ import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppBaseException;
 import pl.lodz.p.it.ssbd2020.ssbd02.mok.facades.AccessLevelFacade;
 import pl.lodz.p.it.ssbd2020.ssbd02.utils.LoggerInterceptor;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 import java.util.List;
@@ -16,12 +20,14 @@ import java.util.List;
 @Interceptors(LoggerInterceptor.class)
 public class AccessLevelManager {
     @Inject
-    AccessLevelFacade accessLevelFacade;
+    private AccessLevelFacade accessLevelFacade;
     /**
      * Metoda, która zwraca wszystkie poziomy dostępu.
      *
      * @return Lista encji AccessLevel
      */
+    @RolesAllowed("editUserAccessLevels")
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public List<AccessLevel> getAllAccessLevels(){
         return accessLevelFacade.findAll();
     }
