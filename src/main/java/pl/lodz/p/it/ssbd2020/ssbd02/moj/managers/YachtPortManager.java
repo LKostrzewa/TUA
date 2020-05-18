1c7df22bbca0d7d0290f3a737164d561055f2016
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2020-05-18 11:01:24 +0200

    implementacja przypadków MOJ.13-15

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/managers/YachtPortManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/managers/YachtPortManager.java
@@ -3,12 +3,18 @@ package pl.lodz.p.it.ssbd2020.ssbd02.moj.managers;
 import pl.lodz.p.it.ssbd2020.ssbd02.entities.Port;
 import pl.lodz.p.it.ssbd2020.ssbd02.entities.Yacht;
 import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppNotFoundException;
+import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.EntityNotActiveException;
+import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.YachtReservedException;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.facades.PortFacade;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.facades.YachtFacade;
 import pl.lodz.p.it.ssbd2020.ssbd02.utils.LoggerInterceptor;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
 import java.util.Collection;
@@ -23,31 +29,64 @@ public class YachtPortManager {
     @Inject
     private YachtFacade yachtFacade;
 
+    /**
+     * Metoda pobierająca wszystkie jachty przypisane do danego portu
+     *
+     * @param portId identyfikator danego portu
+     * @return lista Yacht
+     * @throws AppBaseException wyjątek aplikacyjny, jesli operacja zakończy się niepowodzeniem
+     */
+    @RolesAllowed("getAllYachtsByPort")
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public List<Yacht> getAllYachtsByPort(Long portId) throws AppBaseException {
-        //TODO poprawic na odpowiedni wyjątek
-        Port port = portFacade.find(portId).orElseThrow(() -> new AppBaseException("nie ma tego portu"));
+        Port port = portFacade.find(portId).orElseThrow(AppNotFoundException::createPortNotFoundException);
         return (List<Yacht>) port.getYachts();
     }
 
+    /**
+     * Metoda przypisująca jacht do portu
+     * @param portId identyfikator danego portu
+     * @param yachtId identyfikator danego jachtu
+     * @throws AppBaseException wyjątek aplikacyjny, jesli operacja zakończy się niepowodzeniem
+     */
+    @RolesAllowed("assignYachtToPort")
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void assignYachtToPort(Long portId, Long yachtId) throws AppBaseException {
-        //TODO poprawic na odpowiedni wyjątek
-        Port port = portFacade.find(portId).orElseThrow(() -> new AppBaseException("nie ma tego portu"));
-        Yacht yacht = yachtFacade.find(yachtId).orElseThrow(() -> new AppBaseException("nie ma tego jachtu"));
+        Yacht yacht = yachtFacade.find(yachtId).orElseThrow(AppNotFoundException::createPortNotFoundException);
+        if(!yacht.isActive()){
+            throw EntityNotActiveException.createYachtNotActiveException(yacht);
+        }
+        Port port = portFacade.find(portId).orElseThrow(AppNotFoundException::createPortNotFoundException);
+        if(!port.isActive()){
+            throw EntityNotActiveException.createPortNotActiveException(port);
+        }
         yacht.setCurrentPort(port);
-        Collection<Yacht> yachts = port.getYachts();
+        port.getYachts().add(yacht);
+        /*Collection<Yacht> yachts = port.getYachts();
         yachts.add(yacht);
-        port.setYachts(yachts);
+        port.setYachts(yachts);*/
         portFacade.edit(port);
     }
 
+    /**
+     * Metoda odpisująca jacht z portu
+     * @param portId identyfikator danego portu
+     * @param yachtId identyfikator danego jachtu
+     * @throws AppBaseException wyjątek aplikacyjny, jesli operacja zakończy się niepowodzeniem
+     */
+    @RolesAllowed("retractYachtToPort")
     public void retractYachtFromPort(Long portId, Long yachtId) throws AppBaseException {
-        //TODO poprawic na odpowiedni wyjątek
-        Port port = portFacade.find(portId).orElseThrow(() -> new AppBaseException("nie ma tego portu"));
-        Yacht yacht = yachtFacade.find(yachtId).orElseThrow(() -> new AppBaseException("nie ma tego jachtu"));
+        Yacht yacht = yachtFacade.find(yachtId).orElseThrow(AppNotFoundException::createPortNotFoundException);
+        //TODO pobierać statusy rezerwacji z properties
+        if(yacht.getRentals().stream().anyMatch(r -> r.getRentalStatus().getName().equals("STARTED"))){
+            throw YachtReservedException.createYachtReservedException(yacht);
+        }
+        Port port = portFacade.find(portId).orElseThrow(AppNotFoundException::createPortNotFoundException);
         yacht.setCurrentPort(null);
-        Collection<Yacht> yachts = port.getYachts();
+        port.getYachts().remove(yacht);
+        /*Collection<Yacht> yachts = port.getYachts();
         yachts.remove(yacht);
-        port.setYachts(yachts);
+        port.setYachts(yachts);*/
         portFacade.edit(port);
     }
 }
