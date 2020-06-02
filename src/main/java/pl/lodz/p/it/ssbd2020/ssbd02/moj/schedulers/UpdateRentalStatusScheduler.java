f351b08a6a90ab752ccc73167f004ab2374d73af
216866 Plich Jakub <216866@edu.p.lodz.pl>
2020-06-02 19:15:59 +0200

    poprawki 3

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/schedulers/UpdateRentalStatusScheduler.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/schedulers/UpdateRentalStatusScheduler.java
@@ -6,7 +6,6 @@ import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppBaseException;
 import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppNotFoundException;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.facades.RentalFacade;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.facades.RentalStatusFacade;
-import pl.lodz.p.it.ssbd2020.ssbd02.moj.managers.RentalManager;
 import pl.lodz.p.it.ssbd2020.ssbd02.utils.LoggerInterceptor;
 
 import javax.annotation.security.RolesAllowed;
@@ -31,30 +30,33 @@ public class UpdateRentalStatusScheduler {
     /**
      * Metoda aktualizująca stany rezerwacji. Metoda jest wywoływana codziennie o godzinie 10.00.
      */
-    @Schedule(hour = "10")
+    //@Schedule(hour = "10")
     @RolesAllowed("updateRentalStatus")
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void performTask() throws AppBaseException {
-        List<Rental> allRentals = rentalFacade.findAll();
-        List<RentalStatus> rentalStatuses = rentalStatusFacade.findAll();
-        for (Rental rental : allRentals) {
-            if (rental.getRentalStatus().equals(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
-                    .equals("STARTED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException)) && rental.getEndDate().before(new Date())) {
+    @Schedule(hour = "10")
+    public void performTask() {
+        try {
+            List<Rental> allRentals = rentalFacade.findAll();
+            List<RentalStatus> rentalStatuses = rentalStatusFacade.findAll();
+            for (Rental rental : allRentals) {
+                if (rental.getRentalStatus().equals(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
+                        .equals("STARTED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException)) && rental.getEndDate().before(new Date())) {
 
-                rental.setRentalStatus(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
-                        .equals("FINISHED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException));
+                    rental.setRentalStatus(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
+                            .equals("FINISHED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException));
 
-                rentalFacade.edit(rental);
+                    rentalFacade.edit(rental);
+                }
+                if (rental.getRentalStatus().equals(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
+                        .equals("PENDING")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException)) && rental.getBeginDate().after(new Date())) {
+
+                    rental.setRentalStatus(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
+                            .equals("STARTED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException));
+
+                    rentalFacade.edit(rental);
+                }
             }
-            if (rental.getRentalStatus().equals(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
-                    .equals("PENDING")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException)) && rental.getBeginDate().after(new Date())) {
-
-                rental.setRentalStatus(rentalStatuses.stream().filter(rentalStatus -> rentalStatus.getName()
-                        .equals("STARTED")).findAny().orElseThrow(AppNotFoundException::createRentalStatusNotFoundException));
-
-                rentalFacade.edit(rental);
-            }
+        } catch (AppBaseException e) {
         }
-
     }
 }
