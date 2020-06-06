043cc78213e55d7389c91d13aeddb0e0c35ffdb0
Przemysław Łapiński <216827@edu.p.lodz.pl>
2020-06-06 14:07:16 +0200

    poprawa widoku jachtow po stronie managera

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/web/yacht/YachtDetailsPageBean.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd02/moj/web/yacht/YachtDetailsPageBean.java
@@ -1,8 +1,12 @@
 package pl.lodz.p.it.ssbd2020.ssbd02.moj.web.yacht;
 
 import pl.lodz.p.it.ssbd2020.ssbd02.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd02.moj.dtos.port.PortDetailsDto;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.dtos.yacht.YachtDto;
+import pl.lodz.p.it.ssbd2020.ssbd02.moj.dtos.yachtModel.YachtModelDetailsDto;
+import pl.lodz.p.it.ssbd2020.ssbd02.moj.endpoints.PortEndpoint;
 import pl.lodz.p.it.ssbd2020.ssbd02.moj.endpoints.YachtEndpoint;
+import pl.lodz.p.it.ssbd2020.ssbd02.moj.endpoints.YachtModelEndpoint;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -17,9 +21,16 @@ import java.io.Serializable;
 public class YachtDetailsPageBean implements Serializable {
     @Inject
     private YachtEndpoint yachtEndpoint;
+    @Inject
+    private YachtModelEndpoint yachtModelEndpoint;
+    @Inject
+    private PortEndpoint portEndpoint;
 
     private Long yachtId;
     private YachtDto yachtDto;
+    private String yachtModelName;
+    private String yachtPortName;
+
 
     public Long getYachtId() {
         return yachtId;
@@ -37,10 +48,30 @@ public class YachtDetailsPageBean implements Serializable {
         this.yachtDto = yachtDto;
     }
 
+    public String getYachtModelName() {
+        return yachtModelName;
+    }
+
+    public void setYachtModelName(String yachtModelName) {
+        this.yachtModelName = yachtModelName;
+    }
+
+    public String getYachtPortName() {
+        return yachtPortName;
+    }
+
+    public void setYachtPortName(String yachtPortName) {
+        this.yachtPortName = yachtPortName;
+    }
+
     /**
      * Metoda inicjalizująca komponent.
      */
     public void init() throws AppBaseException {
         this.yachtDto = yachtEndpoint.getYachtById(yachtId);
+        this.yachtModelName = yachtModelEndpoint.getYachtModelById(yachtDto.getYachtModelId()).getModel();
+        this.yachtPortName = (yachtDto.getCurrentPortId() != null) ?
+                portEndpoint.getPortDetailsById(yachtDto.getCurrentPortId()).getName()
+                : null;
     }
 }
